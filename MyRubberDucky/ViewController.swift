1466d4cbe770743588d3ff95da2b2d928a70cbec
Kenny Batista <kennybatista@Tests-MacBook-Pro.local>
2016-07-15 13:06:34 -0400

    initial commit

---
+++ MyRubberDucky/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  RubberDucky
+//
+//  Created by Kenny Batista on 7/15/16.
+//  Copyright © 2016 Kenny Batista. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+}
+
